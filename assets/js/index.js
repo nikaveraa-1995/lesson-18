e2cab9300e286c716348e85d3169fae5913a11f8
nikaveraa-1995 <nika.kataleya@ukr.net>
2025-02-25 12:42:22 +0200

    button

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ import SwipeCarousel from './carousel.swipe.js';
 const carousel = new SwipeCarousel({
   containerID: '.slider',
   slideID: '.item',
-  interval: 1000,
+  interval: 3000,
   // isPlaying: false,
 });
 
